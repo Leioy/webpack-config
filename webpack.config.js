e06334b55e9716380002cd460275e21de3726eb9
leioy <leioy9312@gmail.com>
2022-07-05 18:42:50 +0800

    feat: 提取css

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,15 @@
 const path = require("path")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-const loaders = (...loaders) =>[
-	'style-loader',
-	{
-		loader: 'css-loader',
-		options: {
-			modules: 'icss'
-		}
-	},
-	...loaders
+const loaders = (...loaders) => [
+  MiniCssExtractPlugin.loader,
+  {
+    loader: "css-loader",
+    options: {
+      modules: "icss",
+    },
+  },
+  ...loaders,
 ]
 module.exports = {
   mode: "production",
@@ -17,6 +18,10 @@ module.exports = {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+  },
+  plugins: [new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" })],
+  output: {
+    filename: "[name].[contenthash].js",
   },
   module: {
     rules: [
@@ -27,25 +32,25 @@ module.exports = {
           loader: "babel-loader",
         },
       },
-	    {
-				test: /\.s[ac]ss$/,
-		    exclude: /node_modules/,
-		    use:loaders({
-			    loader:'sass-loader',
-			    options: {
-				    additionalData: `@import "@/scss-vars";`,
-			    }
-		    }),
-	    },
-	    {
-				test: /\.less$/,
-		    use: loaders({
-			    loader:'less-loader',
-			    options: {
-				    additionalData: `@import "@/less-vars";`,
-			    }
-		    })
-	    }
+      {
+        test: /\.s[ac]ss$/,
+        exclude: /node_modules/,
+        use: loaders({
+          loader: "sass-loader",
+          options: {
+            additionalData: `@import "@/scss-vars";`,
+          },
+        }),
+      },
+      {
+        test: /\.less$/,
+        use: loaders({
+          loader: "less-loader",
+          options: {
+            additionalData: `@import "@/less-vars";`,
+          },
+        }),
+      },
     ],
   },
 }
